d8d079edf42cc940a37a65fdd594866381137d0f
anton-marchenko <fattan@ya.ru>
2020-01-27 20:41:15 +0300

    Feat: add auto-generated style header

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+const fs = require('fs').promises;
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const base64 = require('gulp-base64-inline');
+const header = require('gulp-header');
 const browserSync = require('browser-sync').create();
 
 function style() {
@@ -19,13 +21,21 @@ function inlineImage() {
         .pipe(gulp.dest('./css/'))
 }
 
+async function addHeader() {
+    const headerText = await fs.readFile('./style-header.txt', 'utf8');
+
+    return gulp.src('./css/**/*.css')
+        .pipe(header(headerText))
+        .pipe(gulp.dest('./css/'))
+}
+
 function watch() {
     browserSync.init({
         server: {
             baseDir: './'
         }
     });
-    gulp.watch('./scss/**/*.scss', gulp.series(style, inlineImage));
+    gulp.watch('./scss/**/*.scss', gulp.series(style, inlineImage, addHeader));
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
